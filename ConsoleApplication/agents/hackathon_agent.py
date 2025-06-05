54228a941f9f795500656290f890c81a7bb5f54d
karthick <karthi22308@gmail.com>
2025-06-05 11:15:36 +0530

    Final console commit

--- ConsoleApplication/agents/hackathon_agent.py
+++ ConsoleApplication/agents/hackathon_agent.py
@@ -5,9 +5,13 @@ def run_hackathon_agent(theme: str):
                          azure_endpoint='https://hexavarsity-secureapi.azurewebsites.net/api/azureai',
                          api_key='922892c42af122a9')
     prompt = f"Generate a real-world coding challenge for this hackathon theme: {theme}"
-
-    response = client.chat.completions.create(
+    res = client.chat.completions.create(
         model="gpt-4o-mini",
-        messages=[{"role": "user", "content": prompt}]
+        messages=[{'role': 'user', 'content': prompt}],
+        temperature=0.7,
+        max_tokens=2560,
+        top_p=0.6,
+        frequency_penalty=0.7
     )
-    return response.choices[0].message.content
+    return res.choices[0].message.content
+
